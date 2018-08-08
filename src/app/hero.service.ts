f734af5626317766470bf3c699159747126be84b
Sunil <sunil9766@gmail.com>
2018-08-08 16:36:13 +0530

    search component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,6 +16,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class HeroService {
+  
   private heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(
@@ -42,7 +43,7 @@ export class HeroService {
     };
   }
 
-  /** GET heroes from the server */
+  /** GET: heroes from the server */
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
@@ -50,8 +51,22 @@ export class HeroService {
       catchError(this.handleError('getHeroes', []))
     );
   }
+  
+  /** GET hero by id. Return `undefined` when id not found */
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url)
+    .pipe(
+      map(heroes => heroes[0]),  // returns a{0|1} element array
+      tap(h => {
+        const outcome = h ? `fetched` : `did not found`;
+        this.log(`${outcome} hero id=${id}`);
+      }),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
 
-  /** GET hero by id. Will 404 if id not found */
+  /** GET: hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
@@ -60,19 +75,34 @@ export class HeroService {
     );
   }
 
-  /** PUT: update the hero on the server*/
-  updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
-      tap(_ => this.log(`updated hero id=${hero.id}`)),
-      catchError(this.handleError<any>('updateHero'))
+  /** GET: heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if(!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
     );
   }
+
+  // --------Save methods ----------------//
 
   /** POST: add a new hero the server */
   addHero (hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
       tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+  
+  /** PUT: update the hero on the server*/
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
